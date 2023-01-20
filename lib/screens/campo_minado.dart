8262f5cab6eee423f9ed9d45c4acd260974845bf
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2023-01-20 10:27:03 -0300

    assets

--- lib/screens/campo_minado.dart
+++ lib/screens/campo_minado.dart
@@ -1,3 +1,4 @@
+import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -5,12 +6,16 @@ import 'package:flutter/src/widgets/framework.dart';
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
 
+  _reiniciar(){
+    print("Reiniciar");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Campo minado by Playyy")),
+        appBar: ResultadoWidget(venceu: true, onReiniciar: _reiniciar,),
         body: Container(child: Text("Tabuleiro")),
       ),
 
