434bb601a36bb7b84958a99c70f58fe1673ffdc2
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2023-01-23 19:30:49 -0300

    campo_minado_widget

--- lib/screens/campo_minado.dart
+++ lib/screens/campo_minado.dart
@@ -1,7 +1,10 @@
+import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import '../models/campo.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
@@ -10,13 +13,26 @@ class CampoMinadoApp extends StatelessWidget {
     print("Reiniciar");
   }
 
+  _abrir(Campo c){
+    print("abrir");
+  }
+  void _alternarMarcacao(Campo c){
+    print('alternar marcacao');
+  }
+
   @override
   Widget build(BuildContext context) {
+    Campo campo = Campo(linha: 0, coluna: 0);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: ResultadoWidget(venceu: true, onReiniciar: _reiniciar,),
-        body: Container(child: Text("Tabuleiro")),
+        body: Container(child: CampoWidget(
+          campo: campo,
+          onAbrir: _abrir,
+          onAlternarMarcacao: _alternarMarcacao,
+        )),
       ),
 
     );
